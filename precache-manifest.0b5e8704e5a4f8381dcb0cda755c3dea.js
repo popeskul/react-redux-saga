0e082c60cd428cb4aa7b3ab43b356dc1c0d23197
Popeskul Pavlo <peterjj91@gmail.com>
2020-04-04 20:10:15 +0300

    Updates

---
+++ precache-manifest.0b5e8704e5a4f8381dcb0cda755c3dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa10e3b2ce3543e60519664e45460aa3",
+    "url": "/react-redux-saga/index.html"
+  },
+  {
+    "revision": "68ed8e53194cbf6c8d92",
+    "url": "/react-redux-saga/static/js/2.af5feb33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-saga/static/js/2.af5feb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c247820c0f565d0561a2",
+    "url": "/react-redux-saga/static/js/main.f6c56171.chunk.js"
+  },
+  {
+    "revision": "bad6a8cba7d34d5df0ce",
+    "url": "/react-redux-saga/static/js/runtime-main.4b346637.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-saga/precache-manifest.81bee44e124be23dfe0637142acd07a7.js"
+  "/react-redux-saga/precache-manifest.0b5e8704e5a4f8381dcb0cda755c3dea.js"
 );
 
 self.addEventListener('message', (event) => {
